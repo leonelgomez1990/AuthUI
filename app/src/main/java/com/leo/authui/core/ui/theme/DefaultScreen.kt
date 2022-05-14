8ff89420d2d1ebde4286f4de0db8342e10f00c67
leonelgomez1990 <leonelgomez@live.com.ar>
2022-05-14 08:39:15 -0300

    compose theme

---
+++ app/src/main/java/com/leo/authui/core/ui/theme/DefaultScreen.kt
@@ -0,0 +1,19 @@
+package com.leo.authui.core.ui.theme
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Screen(content: @Composable () -> Unit) {
+    DefaultTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background,
+            content = content
+        )
+    }
+}
